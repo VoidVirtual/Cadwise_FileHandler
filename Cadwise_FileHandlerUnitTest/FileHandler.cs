21c1409f8a688a2a14a6e44eed168db39fc75df7
VoidVirtual <58012554+VoidVirtual@users.noreply.github.com>
2020-06-23 22:49:00 +0500

    Add files via upload

---
+++ Cadwise_FileHandlerUnitTest/FileHandler.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows;
+using System.Windows.Data;
+using System.Collections.ObjectModel;
+using Prism.Mvvm;
+namespace Cadwise_FileHandlerUnitTest
+{
+    public partial class FileHandler : BindableBase
+    { 
+        public FileHandler()
+        {
+            m_processesLock = new object();
+            BindingOperations.EnableCollectionSynchronization(m_processes, m_processesLock);
+        }
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Length { get; set; }
+        public bool Removing { get; set; }
+        private readonly object m_processesLock;
+        public ObservableCollection<ProcessData> Processes
+        {
+            get { lock (m_processesLock) return new ObservableCollection<ProcessData>(m_processes.Values); }
+        }
+        public void AddProcess()
+        {
+            int id = m_processes.Count;
+            string title = "Converting " + From + " to " + To;
+            var processData = new ProcessData() { Title = title, Percentage = 0, IsDone = false, ID = id };
+            lock (m_processesLock)
+                m_processes.Add(id, processData);
+            RaisePropertyChanged("Processes");
+            var args = new FiltrationArgs()
+            {
+                From = this.From,
+                To = this.To,
+                Length = this.Length,
+                Removing = this.Removing,
+                ProcessID = id
+            };
+            ThreadPool.QueueUserWorkItem(Filter, args);
+        }
+        public bool IsHandling 
+        { 
+           get 
+           {
+                foreach (var process in m_processes.Values)
+                    if (!process.IsDone && !process.IsAborted)
+                        return true;
+                return false;
+           }
+        }
+        public void Filter(object o)
+        {
+            var args = (FiltrationArgs)o;
+            var temp = m_processes[args.ProcessID];
+            StreamReader reader = null;
+            StreamWriter writer = null;
+            try
+            {
+                var targetDriveName = args.To.Split(new char[] { '\\', '/', ':' })[0];
+                var freeSpace = new DriveInfo(targetDriveName).AvailableFreeSpace;
+                var fileSize = new FileInfo(args.From).Length;
+                if (freeSpace < fileSize)
+                {
+                    throw new Exception("Not enough space for " + args.To);
+                }
+                int bufferSize = (int)Math.Min(m_processRAM, fileSize);
+                reader = new StreamReader(new FileStream(args.From, FileMode.Open, FileAccess.Read), Encoding.UTF8);
+                writer = new StreamWriter(new FileStream(args.To, FileMode.CreateNew, FileAccess.ReadWrite), Encoding.UTF8);
+                var filter = new TextFilter(args.Length, args.Removing, bufferSize);
+                int bytesRead = 0;
+                while (!reader.EndOfStream)
+                {
+                    var readBuf = new char[bufferSize];
+                    reader.Read(readBuf, 0, bufferSize);
+                    bytesRead += Encoding.UTF8.GetByteCount(readBuf);
+                    filter.FilterBuffer(readBuf);
+                    writer.Write(filter.Buffer,0, filter.Size);
+                    int percentage = (int)((double)bytesRead * 100 / fileSize);
+                    temp.Percentage = percentage;
+                    if (percentage >= 100)
+                    {
+                        temp.IsDone = true;
+                    }
+                    lock (m_processesLock)
+                        m_processes[args.ProcessID] = temp;
+                    RaisePropertyChanged("Processes");
+                }
+            }
+            catch (Exception ex)
+            {
+                temp.IsAborted = true;
+                lock (m_processesLock)
+                    m_processes[args.ProcessID] = temp;
+                RaisePropertyChanged("Processes");
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                if(writer!=null)
+                    writer.Dispose();
+                if(reader!=null)
+                   reader.Dispose();
+            }
+        }
+        public void Clear()
+        {
+            foreach (var key in new List<int>(m_processes.Keys))
+            {
+                if (m_processes[key].IsDone || m_processes[key].IsAborted)
+                {
+                    lock (m_processesLock)
+                        m_processes.Remove(key);
+                    RaisePropertyChanged("Processes");
+                }
+            }
+        }
+        private const int m_processRAM = 8192;
+        private Dictionary<int, ProcessData> m_processes = new Dictionary<int, ProcessData>();
+    }
+    public struct FiltrationArgs
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public int Length { get; set; }
+        public bool Removing { get; set; }
+        public int ProcessID { get; set; }
+    }
+    public struct ProcessData
+    {
+        public string Title
+        {
+            get
+            {
+                if (IsDone) 
+                    return title + " (Done)";
+                if (IsAborted) 
+                    return title + " (Aborted)";
+                return title;
+            }
+            set { title = value; }
+        }
+        public bool IsDone
+        {
+            get
+            {
+                if (Percentage == 100) 
+                    isDone = true;
+                return isDone;
+            }
+            set { isDone = value; }
+        }
+        public int Percentage { get; set; }
+        public int ID { get; set; }
+        public bool IsAborted { get; set; }
+        private string title;
+        private bool isDone;
+    }
+}
---
+++ Cadwise_FileHandlerUnitTest/FileHandlerUnitTest.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using Cadwise_FileHandlerUnitTest;
+
+namespace Cadwise_FileHandlerUnitTest
+{
+    public static class CustomAssert
+    {
+        public static void AreEqual(char[] b1, char[] b2)
+        {
+            for (int i = 0; i < b1.Length && i < b2.Length; i++)
+            {
+                if (b1[i] != b2[i])
+                    Assert.IsTrue(false);
+            }
+            if (b1.Length < b2.Length)
+            {
+                if (b2[b1.Length] != '\0')
+                    Assert.IsTrue(false);
+            }
+            if (b2.Length < b1.Length)
+            {
+                if (b1[b2.Length] != '\0')
+                    Assert.IsTrue(false);
+            }
+            Assert.IsTrue(true);
+        }
+        public static void AreEqual(FilterableBuffer b1, FilterableBuffer b2)
+        {
+            if (!b1.CurrentWordSymbols.SequenceEqual(b2.CurrentWordSymbols))
+                Assert.IsTrue(false);
+            AreEqual(b1.Buffer, b2.Buffer);
+        }
+    }
+    [TestClass]
+    public class FileHandlerUnitTest
+    {
+        public static void AssertBufferFilter(int wordLength, bool removing, char[] original, FilterableBuffer primer)
+        {
+            var filter = new TextFilter(wordLength, removing, original.Length);
+            filter.FilterBuffer(original);
+            CustomAssert.AreEqual(filter, primer);
+        }
+        public static void AssertTwoBuffersFilter(int wordLength, bool removing, Tuple<char[], char[]> originals, Tuple<char[], char[]> primers)
+        {
+            var filter = new TextFilter(wordLength, removing, originals.Item1.Length);
+            filter.FilterBuffer(originals.Item1);
+            CustomAssert.AreEqual(filter.Buffer, primers.Item1);
+            filter.FilterBuffer(originals.Item2);
+            CustomAssert.AreEqual(filter.Buffer, primers.Item2);
+        }
+        [TestMethod]
+        public void TestBufferFilter1()
+        {
+            var primer = new FilterableBuffer();
+            primer.Buffer = new char[] { 'a', 'b', 'c', ' ', '\n',
+                                         'h', 'j', 'k',
+                                       };
+            char[] original = new char[] { 'a', 'b', 'c', ' ', 'd', 'e', '\n',
+                                         'f','g', ' ', 'h', 'j', 'k',
+                                       };
+            AssertBufferFilter(3, false, original, primer);
+        }
+        [TestMethod]
+        public void TestBufferFilter2()
+        {
+            var primer = new FilterableBuffer();
+            primer.CurrentWordSymbols = new Queue<char>(new[] { 'h', 'j' });
+            primer.Buffer = new char[] { 'a', 'b', 'c', ' ', '\n',
+
+                                       };
+            char[] original = new char[] { 'a', 'b', 'c', ' ', 'e', 'd', '\n',
+                                         'f','g', ' ', 'h', 'j',
+                                       };
+            AssertBufferFilter(3, false, original, primer);
+        }
+        [TestMethod]
+        public void TestBufferFilter_Punctuation1()
+        {
+            var primer = new FilterableBuffer();
+            primer.Buffer = new char[] { 'a' };
+            char[] original = new char[] { '(', 'a', ')', };
+            AssertBufferFilter(0, true, original, primer);
+        }
+        [TestMethod]
+        public void TestBufferFilter_Punctuation2()
+        {
+            var primer = new FilterableBuffer();
+            primer.Buffer = new char[] { };
+            primer.CurrentWordSymbols = new Queue<char> { };
+            char[] original = new char[] { '(', 'a', ')', };
+            AssertBufferFilter(2, true, original, primer);
+        }
+        [TestMethod]
+        public void TestBufferFilter_Punctuation3()
+        {
+            var primer = new FilterableBuffer();
+            primer.Buffer = new char[] { 'a', ' ', 'b', };
+            primer.CurrentWordSymbols = new Queue<char> { };
+            char[] original = new char[] { 'a', ',', ' ', 'b', };
+            AssertBufferFilter(0, true, original, primer);
+        }
+        [TestMethod]
+        public void TestTwoBuffersFilter1()
+        {
+            var original1 = new char[] { 'a', 'b', 'c', ' ', 'd' };
+            var original2 = new char[] { 'e', 'f', };
+            var primer1 = new char[] { 'a', 'b', 'c', ' ' };
+            var primer2 = new char[] { 'd', 'e', 'f', };
+            AssertTwoBuffersFilter(3, false, new Tuple<char[], char[]>(original1, original2), new Tuple<char[], char[]>(primer1, primer2));
+        }
+        [TestMethod]
+        public void TestTwoBuffersFilter2()
+        {
+            var original1 = new char[] { 'a', 'b', 'c', 'd' };
+            var original2 = new char[] { 'e', 'f', };
+            var primer1 = new char[] { 'a', 'b', 'c', 'd' };
+            var primer2 = new char[] { 'e', 'f', };
+            AssertTwoBuffersFilter(3, false, new Tuple<char[], char[]>(original1, original2), new Tuple<char[], char[]>(primer1, primer2));
+        }
+        [TestMethod]
+        public void TestTwoBuffersFilter_Punctuation()
+        {
+            var original1 = new char[] { 'a', 'b', 'c', ' ', '(', 'e' };
+            var original2 = new char[] { ')' };
+            var primer1 = new char[] { 'a', 'b', 'c', ' ' };
+            var primer2 = new char[] { };
+            AssertTwoBuffersFilter(3, true, new Tuple<char[], char[]>(original1, original2), new Tuple<char[], char[]>(primer1, primer2));
+        }
+        [TestMethod]
+        public void TestFilterBufferTwoSpaces()
+        {
+            var primer = new FilterableBuffer();
+            primer.Buffer = new char[] { 'a', ' ', ' ', 'b', };
+            char[] original = new char[] { 'a', ' ', ' ', 'b', };
+            AssertBufferFilter(1, false, original, primer);
+        }
+        [TestMethod]
+        public void TestFileHandlerException1()
+        {
+            var fileHandler = new FileHandler();
+            var args = new FiltrationArgs
+            {
+                From = "C:/NonExistingFile1.txt",
+                To = "F:/NonExistingFile2.txt",
+                Removing = false,
+                Length = 0,
+                ProcessID = 0
+            };
+            try
+            {
+                fileHandler.Filter(args);
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+        [TestMethod]
+        public void TestFileHandlerException2()
+        {
+            var fileHandler = new FileHandler();
+            var args = new FiltrationArgs
+            {
+                From = "C:/Over32GBSizefile.txt",
+                To = "DRIVE4GB:/To.txt",
+                Removing = false,
+                Length = 0,
+                ProcessID = 0
+            };
+            try
+            {
+                fileHandler.Filter(args);
+                Assert.IsTrue(false);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+    }
+}
